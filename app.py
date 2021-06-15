d95a8f9c92ed3fe613e8a69e943db2621342f847
samuel <sam.castle02@gmail.com>
2021-06-15 11:47:32 +0000

    latest

--- app.py
+++ app.py
@@ -7,10 +7,51 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:02November19961!@3
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-class Users(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(30), nullable=False)
-    last_name = db.Column(db.String(30), nullable=False)
+############################# INTRO - making a working website ###########################################################
+
+#from flask import Flask
+
+#app = Flask(__name__)
+
+#@app.route('/')
+#@app.route('/home')
+#def hpme(): 
+#    return "Home page"
+
+# @app.route('/about')
+# def about_page():
+#    return "About page" 
+
+#@app.route('/squared/<num>')
+#def squared(num):
+#    number = int(num)
+#    result = number*number
+#    return str(result)
+
+#if __name__=='__main__':
+#    app.run(debug=True)
+
+# indpent lines of code, i have hashed out the first and last 2 lines (as they are repeats)
+
+#################################### old - Cities, Countries ########################################
+
+#class Countries(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    name = db.Column(db.String(30), nullable=False)
+#    cities = db.relationship('Cities', backref='country') 
+
+#class Cities(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    name = db.Column(db.String(30), nullable=False)
+#    country_id = db.Column(db.Integer, db.ForeignKey('countries.id'), nullable=False)
+
+################################## old - Users ##########################################
+
+#class Users(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    first_name = db.Column(db.String(30), nullable=False)
+#    last_name = db.Column(db.String(30), nullable=False)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- create.py
+++ create.py
@@ -1,10 +1,27 @@
-from app import db, Users 
-# from app import Users - as alternative 
+from app import      #db, Countries, Cities     #db, Users 
+## from app import Users - as alternative 
 
 db.drop_all()                      #- deletes existing ables 
 db.create_all()                    #- re-populates ables 
 
-test_user = Users(first_name='Grooty',last_name='Toot') # Example entry
-db.session.add(test_user)           
+
+
+###################### old - Cities + Countries #####################################################
+
+#UK = Countries(name = 'United Kingdom') #Add example to countries table
+#db.session.add(UK)
+#db.session.commit()
+
+
+#ldn = Cities(name='London', country = UK)
+#mcr = Cities(name='Manchester', country = Countries.query.filter_by(name='United Kingdom').first())
+
+#db.session.add(ldn)
+#db.session.add(mcr)
+
+####################### old - Users #####################################################
+
+##test_user = Users(first_name='Grooty',last_name='Toot') # Example entry
+##db.session.add(test_user)           
 
 db.session.commit()
