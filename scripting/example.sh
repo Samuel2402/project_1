fa723f927bd8a942e74306226685eb1a65f79e50
samuel <sam.castle02@gmail.com>
2021-06-22 11:12:55 +0000

    new

--- scripting/example.sh
+++ scripting/example.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-echo "Hello, what is your user name?"
-read username
-currentuser="$(whoami)"
-if [ $username == $currentuser ]; then
-        echo "You are the current logged in user"
-else
-        echo "You are not logged in"
-fi
+#echo "Hello, what is your user name?"
+#read username
+#currentuser="$(whoami)"
+#if [ $username == $currentuser ]; then
+#        echo "You are the current logged in user"
+#else
+#        echo "You are not logged in"
+#fi
 
 ################################# first #########################################
 
